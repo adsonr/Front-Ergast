785267e6f1399af344db18de57fdcbd8540a5081
adsonr <adsonufc@gmail.com>
2017-08-27 23:17:51 -0300

    FIX: adicionando timeout ao slidershow

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ $(document).ready(function() {
     var $slideContainer = $('.slides', $slider);
     var $slides = $('.slide', $slider);
 
-    var interval;
+    var interval, timeout;
 
     function startSlider() {
+        console.log('startSlider');
         interval = setInterval(function() {
+            console.log('executeSlider');
             $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
                 if (++currentSlide === $slides.length) {
                     currentSlide = 1;
@@ -25,15 +27,36 @@ $(document).ready(function() {
             });
         }, pause);
     }
+
+    function nextSlider() {
+        console.log('nextSlider');
+        $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+            console.log('nextSlider callback');
+            if (++currentSlide === $slides.length) {
+                currentSlide = 1;
+                $slideContainer.css('margin-left', 0);
+            }
+            timeout = setTimeout(function() {
+                nextSlider();
+            }, pause);
+        });
+    }
+
     function pauseSlider() {
+        console.log('pauseSlider');
         clearInterval(interval);
+        clearTimeout(timeout);
     }
 
     $slideContainer
         .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
+        .on('mouseleave', startSlider2);
 
-    startSlider();
+    startSlider2();
 
-
+    function startSlider2() {
+        timeout = setTimeout(function() {
+            nextSlider();
+        }, pause);
+    }
 });
