d823b45d8969606182d0cb76c9dd275b50f8b914
Adson Rodrigues <adson.rodrigues@ifnet-for.ifactory.com.br>
2017-08-29 18:26:53 -0300

    FIX: atualizado consumo de API na season-list.html e request feito em season.js

--- js/season.js
+++ js/season.js
@@ -1,6 +1,24 @@
-fetch('http://ergast.com/api/f1/seasons', {
+fetch('http://ergast.com/api/f1/seasons.json', {
     method: 'GET'
 })
-    .then(response) {
+    .then(
+    function (response) {
+        if (response.status !== 200) {
+            console.log('Ocorreu um erro. Status Code: ' + response.status);
+            return;
+        }
 
-}
+        // resposta 
+        response.json().then(function (data) {
+            var seasons = data.MRData.SeasonTable.Seasons;
+            seasons.forEach(function (element) {
+                var seasonEl = document.createElement('li');
+                seasonEl.innerHTML = `${element.season} <a href="${element.url}">${element.url}</a>`;
+                document.getElementById('season-list').appendChild(seasonEl);
+            });
+        });
+    }
+    )
+    .catch(function (err) {
+        console.log('Fetch Error: ', err);
+    });
